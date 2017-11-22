ed4966ca1457562e987bdf76190017f97e14a1a7
Adrien Floor <adriaandrum@gmail.com>
2017-11-22 20:14:15 +0200

    architecture + form + static pages + shopify set up

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,37 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def adventure
+  end
+
+  def shop
+  end
+
+  def first_product
+
+  end
+
+  def second_product
+
+  end
+
+  def third_product
+
+  end
+
+  def about_road
+
+  end
+
+  def about_brand
+
+  end
+
+  def faq
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+
+  #home
+  root to:'pages#home'
+
+  #about
+  get 'about', to: 'pages#about'
+  get 'about_road', to: 'pages#about_road'
+  get 'about_brand', to: 'pages#about_brand'
+
+  #adventure
+  get 'adventure', to: 'pages#adventure'
+
+  #shop
+  get 'shop', to: 'pages#shop'
+
+  get 'first_product', to: 'pages#first_product'
+  get 'second_product', to: 'pages#second_product'
+  get 'third_product', to: 'pages#third_product'
+
+  #FAQ
+  get 'faq', to: 'pages#faq'
+
+  #contact
   get 'contact-me', to: 'messages#new', as: 'new_message'
   post 'contact-me', to: 'messages#create', as: 'create_message'
 end
